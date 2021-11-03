ec6e2aed37f8a9f377f270b49b0c68bfb954bf53
Emmanuelle <Emmanuelle.GOUGELET@berger-levrault.com>
2021-11-03 14:49:36 +0100

    start summary tools commente

--- App/ooka_tools.py
+++ App/ooka_tools.py
@@ -1,14 +1,20 @@
 import os
 
 
-"""
- pour remplacer le os.environement et cree un message d'erreur
-"""
 def get_env_variable(name):
-    try:
-        return os.environ[name]
-    except KeyError:
-        message = "Expected environment variable '{}' not set.".format(name)
-        raise Exception(message)
+   """[summary]
 
+   Args:
+       name ([type]): [description]
 
+   Raises:
+       Exception: [description]
+
+   Returns:
+       [type]: [description]
+   """
+   try:
+      return os.environ[name]
+   except KeyError:
+      message = "Expected environment variable '{}' not set.".format(name)
+      raise Exception(message)
