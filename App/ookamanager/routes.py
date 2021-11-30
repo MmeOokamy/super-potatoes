e85c997d1f414c1bd42937d5fc2dae1313f5e64c
MmeOokamy <manue.ggt@gmail.com>
2021-11-30 16:35:55 +0100

    ookamanager ajout tache

--- App/ookamanager/routes.py
+++ App/ookamanager/routes.py
@@ -5,13 +5,13 @@ from flask import (
 )
 
 from .. import login_manager
-from .models import db, Projects, Modules, Steps, Status
-from .forms import ProjectForm, ModuleForm
+from .models import db, Projects, Modules, Steps, Status, Tasks
+from .forms import ProjectForm, ModuleForm, TaskForm
 
 
 om_bp = Blueprint('ookamanager', __name__,
-    url_prefix='/ookamanager',
-    template_folder='templates'
+    url_prefix= '/ookamanager',
+    template_folder= 'templates'
 )
 
 NAME_MENU= 'Ookamanager'
@@ -20,7 +20,7 @@ NAME_MENU= 'Ookamanager'
 @login_required
 def om_index():
     projects = Projects.query.filter_by(project_user_id=current_user.id).all()
-    return render_template('om_i.html', menu_active=NAME_MENU,  projects=projects)
+    return render_template('om_i.html', menu_active= NAME_MENU,  projects= projects)
 
 
 @om_bp.route('/projet', methods=['GET', 'POST'])
@@ -43,8 +43,8 @@ def om_project():
         flash('Ce projet existe déjà')
     return render_template(
         'om_project_form.jinja2',
-        form=form,
-        menu_active=NAME_MENU
+        form= form,
+        menu_active= NAME_MENU
     )
 
 
@@ -66,9 +66,9 @@ def om_module():
         flash('Ce module existe déjà')
     return render_template(
         'om_module_form.jinja2',
-        menu_active=NAME_MENU,
-        form=form,
-        modules=modules
+        menu_active= NAME_MENU,
+        form= form,
+        modules= modules
     )
 
 
@@ -84,26 +84,46 @@ def om_dashboard(dashboard_id):
     if dashboard is not None and dashboard.project_user_id == current_user.id:
         return render_template(
             'om_dashboard.html',
-            dashboard = dashboard,
-            steps = steps,
-            menu_active=NAME_MENU
+            dashboard= dashboard,
+            steps= steps,
+            menu_active= NAME_MENU
         )
     else:
         # redirection si on entre une numero d'id inconnue
         return redirect(url_for('ookamanager.om_index'))
     
 
-@om_bp.route('/dashboard/<dashboard_id>/todo', methods=['GET', 'POST'])
+@om_bp.route('/task', methods=['GET', 'POST'])
 @login_required
-def om_add_todo(dashboard_id):
-    dashboard = Projects.query.filter_by(id=dashboard_id).first()
-    if dashboard is not None:
-        return render_template(
-            'om_dashboard.html',
-            dashboard = dashboard,
-            menu_active=NAME_MENU
-        )
+def om_add_todo():
+    project_id = request.args.get('dashboard_id')
+    modules_liste = Modules.query.all()
+    form = TaskForm()
+
+    # Pour remplir le selectfield \o/ avec la liste de thème
+    form.task_module.choices = [(m.id, m.module_name) for m in Modules.query.order_by('module_name')]
+
+    if form.validate_on_submit():
+        existing_theme = Tasks.query.filter_by(task=form.task_title.data).first()
+        if existing_theme is None:
+            tasks = Task(
+                task_title= form.task_title.data,
+                task_body= form.task_body.data,
+                task_order= 0,
+                task_project_id= project_id,
+                task_module= form.task_module.data,
+                task_deadline= form.task_deadline.data,
+                task_step_id= 1,
+                task_user_id= current_user.id,
+            )
+            db.session.add(task)
+            db.session.commit()  # Create new theme
+            return redirect(url_for('ookamanager.om_dashboard', dashboard_id=project_id))
+        flash(' euh Je sais po')
+
     return render_template(
-            'om_dashboard.html',
-            menu_active=NAME_MENU
+        'om_task_form.jinja2',
+        form= form,
+        p_id= project_id,
+        menu_active= NAME_MENU
     )
